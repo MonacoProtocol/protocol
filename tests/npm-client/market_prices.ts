af13e3ac09213681df960d3e94c5a4ff54676468
Drew Spencer <druspencer@gmail.com>
2023-01-16 15:07:58 +0000

    chore: align npm client code with released code

---
+++ tests/npm-client/market_prices.ts
@@ -0,0 +1,118 @@
+import { Program } from "@project-serum/anchor";
+import assert from "assert";
+import { getMarketPrices } from "../../npm-client/src";
+import { monaco } from "../util/wrappers";
+import { createWalletWithBalance } from "../util/test_util";
+
+describe("Market Prices", () => {
+  const prices = [2.0, 3.0];
+  const stake = 10000000;
+  const stakeSimple = 10.0;
+
+  it("Market no prices", async () => {
+    const market = await monaco.create3WayMarket(prices);
+
+    const response = await getMarketPrices(
+      monaco.program as Program,
+      market.pk,
+    );
+
+    assert(response.success);
+    assert(response.data.market);
+    assert(response.data.marketOutcomeAccounts);
+    assert.deepEqual(response.errors, []);
+    assert.deepEqual(response.data.pendingOrders, []);
+    assert.deepEqual(response.data.marketPrices, []);
+    assert.deepEqual(response.data.marketOutcomeAccounts.length, 3);
+  });
+
+  it("Market single price per outcome", async () => {
+    const market = await monaco.create3WayMarket(prices);
+
+    const purchaser = await createWalletWithBalance(monaco.provider);
+    await market.airdrop(purchaser, stake);
+    await Promise.all([
+      market.forOrder(0, stakeSimple, prices[0], purchaser),
+      market.forOrder(1, stakeSimple, prices[0], purchaser),
+      market.forOrder(2, stakeSimple, prices[0], purchaser),
+    ]);
+
+    const response = await getMarketPrices(
+      monaco.program as Program,
+      market.pk,
+    );
+
+    assert.deepEqual(response.data.pendingOrders.length, 3);
+    assert.deepEqual(response.data.marketPrices.length, 3);
+    assert.deepEqual(
+      response.data.pendingOrders[0].account.stakeUnmatched.toNumber(),
+      stake,
+    );
+    assert.deepEqual(
+      response.data.pendingOrders[0].account.stake.toNumber(),
+      stake,
+    );
+  });
+
+  it("Market multiple prices for outcome", async () => {
+    const market = await monaco.create3WayMarket(prices);
+
+    const [purchaser, purchaser2] = await Promise.all([
+      createWalletWithBalance(monaco.provider),
+      createWalletWithBalance(monaco.provider),
+    ]);
+    await Promise.all([
+      market.airdrop(purchaser, stake),
+      market.airdrop(purchaser2, stake),
+    ]);
+    await Promise.all([
+      market.forOrder(0, stakeSimple, prices[0], purchaser),
+      market.forOrder(0, stakeSimple, prices[1], purchaser),
+      market.forOrder(0, stakeSimple, prices[0], purchaser2),
+    ]);
+
+    const response = await getMarketPrices(
+      monaco.program as Program,
+      market.pk,
+    );
+
+    assert(response.success);
+    assert.deepEqual(response.errors, []);
+    assert.deepEqual(response.data.pendingOrders.length, 3);
+    assert.deepEqual(response.data.marketPrices.length, 2);
+  });
+
+  it("Market partially matched and fully matched", async () => {
+    const market = await monaco.create3WayMarket(prices);
+
+    const [purchaser, purchaser2] = await Promise.all([
+      createWalletWithBalance(monaco.provider),
+      createWalletWithBalance(monaco.provider),
+    ]);
+    await Promise.all([
+      market.airdrop(purchaser, stake),
+      market.airdrop(purchaser2, stake),
+    ]);
+    const [order1, order2] = await Promise.all([
+      market.forOrder(0, stakeSimple, prices[0], purchaser),
+      market.againstOrder(0, stakeSimple + 1, prices[0], purchaser2),
+    ]);
+
+    await market.match(order1, order2);
+    await new Promise((e) => setTimeout(e, 1000));
+
+    const response = await getMarketPrices(
+      monaco.program as Program,
+      market.pk,
+    );
+
+    assert(response.success);
+    assert.deepEqual(response.errors, []);
+    assert.deepEqual(response.data.pendingOrders.length, 1);
+    assert.deepEqual(response.data.marketPrices.length, 1);
+    assert.deepEqual(
+      response.data.pendingOrders[0].account.stakeUnmatched.toNumber(),
+      1000000,
+    );
+  });
+});
--- tests/npm-client/order_query.ts
+++ tests/npm-client/order_query.ts
@@ -1,3 +1,4 @@
+import { PublicKey } from "@solana/web3.js";
 import assert from "assert";
 import {
   getOrdersByMarketForProviderWallet,
@@ -5,14 +6,19 @@ import {
   getOrdersByEventForProviderWallet,
   getCancellableOrdersByMarketForProviderWallet,
   OrderStatus,
+  Orders,
 } from "../../npm-client/src";
 import { createOrderUiStake as createOrderNpm } from "../../npm-client/src/create_order";
 import { monaco } from "../util/wrappers";
+import { createWalletWithBalance } from "../util/test_util";
 
 describe("Order Query", () => {
-  const outcomeIndex = 1;
+  const outcomeA = 0;
+  const outcomeB = 1;
   const price = 6.0;
   const stake = 2000;
+
+  const pkBase58 = (publicKey: PublicKey) => publicKey.toBase58();
 
   it("Gets order", async () => {
     const market = await monaco.create3WayMarket([price]);
@@ -21,7 +27,7 @@ describe("Order Query", () => {
     const createOrderResponse = await createOrderNpm(
       monaco.getRawProgram(),
       market.pk,
-      outcomeIndex,
+      outcomeA,
       true,
       price,
       stake,
@@ -69,5 +75,109 @@ describe("Order Query", () => {
     assert(responseCancellable.data);
     assert.equal(responseCancellable.data.orderAccounts.length, 1);
     assert.deepEqual(responseCancellable.errors, []);
+  });
+
+  it("filterByMarket", async () => {
+    const [purchaser, market1, market2] = await Promise.all([
+      createWalletWithBalance(monaco.provider),
+      monaco.create3WayMarket([price]),
+      monaco.create3WayMarket([price]),
+    ]);
+    await market1.airdrop(purchaser, 100.0);
+    await market2.airdrop(purchaser, 100.0);
+
+    const order1Pk = await market1.forOrder(outcomeA, 20.0, price, purchaser);
+    const order2Pk = await market2.forOrder(outcomeA, 20.0, price, purchaser);
+
+    // fetch orders for market "1"
+    const response1 = await Orders.orderQuery(monaco.getRawProgram())
+      .filterByMarket(market1.pk)
+      .fetchPublicKeys();
+    assert(response1.success);
+    assert.deepEqual(response1.errors, []);
+    assert.deepEqual(response1.data.publicKeys.map(pkBase58), [
+      order1Pk.toBase58(),
+    ]);
+
+    // fetch orders for market "2"
+    const response2 = await Orders.orderQuery(monaco.getRawProgram())
+      .filterByMarket(market2.pk)
+      .fetchPublicKeys();
+    assert(response2.success);
+    assert.deepEqual(response2.errors, []);
+    assert.deepEqual(response2.data.publicKeys.map(pkBase58), [
+      order2Pk.toBase58(),
+    ]);
+  });
+
+  it("filterByMarketOutcomeIndex", async () => {
+    const [purchaser, market] = await Promise.all([
+      createWalletWithBalance(monaco.provider),
+      monaco.create3WayMarket([price]),
+    ]);
+    await market.airdrop(purchaser, 100.0);
+
+    const order1Pk = await market.forOrder(outcomeA, 20.0, price, purchaser);
+    const order2Pk = await market.forOrder(outcomeB, 20.0, price, purchaser);
+
+    // fetch orders for outcome "A"
+    const response1 = await Orders.orderQuery(monaco.getRawProgram())
+      .filterByMarket(market.pk)
+      .filterByMarketOutcomeIndex(outcomeA)
+      .fetchPublicKeys();
+    assert(response1.success);
+    assert.deepEqual(response1.errors, []);
+    assert.deepEqual(response1.data.publicKeys.map(pkBase58), [
+      order1Pk.toBase58(),
+    ]);
+
+    // fetch orders for outcome "B"
+    const response2 = await Orders.orderQuery(monaco.getRawProgram())
+      .filterByMarket(market.pk)
+      .filterByMarketOutcomeIndex(outcomeB)
+      .fetchPublicKeys();
+    assert(response2.success);
+    assert.deepEqual(response2.errors, []);
+    assert.deepEqual(response2.data.publicKeys.map(pkBase58), [
+      order2Pk.toBase58(),
+    ]);
+  });
+
+  it("filterByForOutcome", async () => {
+    const [purchaser, market] = await Promise.all([
+      createWalletWithBalance(monaco.provider),
+      monaco.create3WayMarket([price]),
+    ]);
+    await market.airdrop(purchaser, 100.0);
+
+    const order1Pk = await market.forOrder(outcomeA, 20.0, price, purchaser);
+    const order2Pk = await market.againstOrder(
+      outcomeA,
+      20.0,
+      price,
+      purchaser,
+    );
+
+    // fetch orders for market "1" and for outcome "A"
+    const response1 = await Orders.orderQuery(monaco.getRawProgram())
+      .filterByMarket(market.pk)
+      .filterByForOutcome(true)
+      .fetchPublicKeys();
+    assert(response1.success);
+    assert.deepEqual(response1.errors, []);
+    assert.deepEqual(response1.data.publicKeys.map(pkBase58), [
+      order1Pk.toBase58(),
+    ]);
+
+    // fetch orders for market "1" and against outcome "A"
+    const response2 = await Orders.orderQuery(monaco.getRawProgram())
+      .filterByMarket(market.pk)
+      .filterByForOutcome(false)
+      .fetchPublicKeys();
+    assert(response2.success);
+    assert.deepEqual(response2.errors, []);
+    assert.deepEqual(response2.data.publicKeys.map(pkBase58), [
+      order2Pk.toBase58(),
+    ]);
   });
 });
