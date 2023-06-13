553418c8d9b8266fc7923b79cc3701a506e5d2d9
Pawel Witek <pawel@betdex.com>
2023-06-13 17:10:56 +0100

    another try

--- programs/monaco_protocol/src/instructions/market_position/update_on_order_match.rs
+++ programs/monaco_protocol/src/instructions/market_position/update_on_order_match.rs
@@ -14,23 +14,23 @@ pub fn update_on_order_match(
 
     let outcome_index = order.market_outcome_index as usize;
     let for_outcome = order.for_outcome;
-    let unmatched_price = order.expected_price;
+    let price_unmatched = order.expected_price;
 
     // update chosen outcome position
-    let matched_risk = calculate_risk_from_stake(stake_matched, price_matched);
-    let unmatched_risk = calculate_risk_from_stake(stake_matched, unmatched_price);
+    let risk_pre_match = calculate_risk_from_stake(stake_matched, price_unmatched);
+    let risk_post_match = calculate_risk_from_stake(stake_matched, price_matched);
 
     match for_outcome {
         true => {
             market_position.market_outcome_sums[outcome_index] = market_position
                 .market_outcome_sums[outcome_index]
-                .checked_add(matched_risk as i128)
+                .checked_add(risk_post_match as i128)
                 .ok_or(CoreError::ArithmeticError)?;
         }
         false => {
             market_position.market_outcome_sums[outcome_index] = market_position
                 .market_outcome_sums[outcome_index]
-                .checked_sub(matched_risk as i128)
+                .checked_sub(risk_post_match as i128)
                 .ok_or(CoreError::ArithmeticError)?;
         }
     }
@@ -75,7 +75,7 @@ pub fn update_on_order_match(
         false => {
             market_position.unmatched_exposures[outcome_index] = market_position
                 .unmatched_exposures[outcome_index]
-                .checked_sub(unmatched_risk)
+                .checked_sub(risk_pre_match)
                 .ok_or(CoreError::ArithmeticError)?;
         }
     }
