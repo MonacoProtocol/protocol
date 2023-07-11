7a2b4463451330936a2f5322e54d0315f8fece74
Drew Spencer <druspencer@gmail.com>
2023-07-11 16:08:58 +0100

    chore: retrun original import

--- programs/monaco_protocol/src/lib.rs
+++ programs/monaco_protocol/src/lib.rs
@@ -4,9 +4,8 @@ use crate::context::*;
 use crate::error::CoreError;
 use crate::instructions::market::verify_market_authority;
 use crate::instructions::verify_operator_authority;
-use crate::state::market_account::MarketStatus::*;
 use crate::state::market_account::{
-    Market, MarketMatchingPool, MarketOrderBehaviour, MarketOutcome,
+    Market, MarketMatchingPool, MarketOrderBehaviour, MarketOutcome, MarketStatus::ReadyToClose,
 };
 use crate::state::market_position_account::MarketPosition;
 use crate::state::market_type::verify_market_type;
