058a81596fc2518f3e48fa599a5356542fa0e21c
Dru (he/him) <druspencer@gmail.com>
2023-09-19 15:55:08 +0100

    Fix/only transition open inplay markets (#105)

--- programs/monaco_protocol/src/instructions/market/mod.rs
+++ programs/monaco_protocol/src/instructions/market/mod.rs
@@ -1,6 +1,7 @@
 mod create_market;
 mod escrow;
 mod market_authority;
+mod move_to_inplay;
 mod update_market_event_start_time;
 mod update_market_locktime;
 mod update_market_status;
@@ -9,6 +10,7 @@ mod update_market_title;
 pub use create_market::*;
 pub use escrow::*;
 pub use market_authority::*;
+pub use move_to_inplay::*;
 pub use update_market_event_start_time::*;
 pub use update_market_locktime::*;
 pub use update_market_status::*;
--- programs/monaco_protocol/src/lib.rs
+++ programs/monaco_protocol/src/lib.rs
@@ -422,24 +422,8 @@ pub mod monaco_protocol {
     }
 
     pub fn move_market_to_inplay(ctx: Context<UpdateMarketUnauthorized>) -> Result<()> {
-        let now = current_timestamp();
         let market = &mut ctx.accounts.market;
-
-        // market must have inplay enabled
-        require!(market.inplay_enabled, CoreError::MarketInplayNotEnabled);
-
-        // set it `true` only if it's `false`
-        require!(!market.inplay, CoreError::MarketAlreadyInplay);
-
-        // set it `true` only if now is after event start
-        require!(
-            market.event_start_timestamp <= now,
-            CoreError::MarketEventNotStarted,
-        );
-
-        market.inplay = true;
-
-        Ok(())
+        instructions::market::move_market_to_inplay(market)
     }
 
     pub fn open_market(ctx: Context<UpdateMarket>) -> Result<()> {
