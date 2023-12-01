6019e6401d040fdded0bf042172ad1f5df659631
Eoin Murphy <95632764+eoin-betdex@users.noreply.github.com>
2023-12-01 10:19:04 +0000

    feat: instruction to cancel unmatched liquidity after market lock (#141)

--- tests/util/wrappers.ts
+++ tests/util/wrappers.ts
@@ -227,6 +227,7 @@ export class Monaco {
     eventStartTimestamp?: number,
     marketLockTimestamp?: number,
     eventStartOrderBehaviour?: object,
+    marketLockOrderBehaviour?: object,
   ) {
     const market = await this.createMarket(
       ["TEAM_1_WIN", "DRAW", "TEAM_2_WIN"],
@@ -237,6 +238,7 @@ export class Monaco {
       eventStartTimestamp,
       marketLockTimestamp,
       eventStartOrderBehaviour,
+      marketLockOrderBehaviour,
     );
     await market.open();
     return market;
@@ -267,6 +269,7 @@ export class Monaco {
     inplayEnabled?: boolean;
     inplayOrderDelay?: number;
     eventStartOrderBehaviour?: object;
+    marketLockOrderBehaviour?: object;
     marketOperatorKeypair?: Keypair;
   }) {
     /* eslint-disable */
@@ -303,6 +306,9 @@ export class Monaco {
     const eventStartOrderBehaviour = options.eventStartOrderBehaviour
       ? options.eventStartOrderBehaviour
       : { cancelUnmatched: {} };
+    const marketLockOrderBehaviour = options.marketLockOrderBehaviour
+      ? options.marketLockOrderBehaviour
+      : { none: {} };
     // prettier-ignore-end
     /* eslint-enable */
 
@@ -346,7 +352,7 @@ export class Monaco {
         inplayEnabled,
         inplayOrderDelay,
         eventStartOrderBehaviour,
-        { none: {} },
+        marketLockOrderBehaviour,
       )
       .accounts({
         existingMarket: null,
@@ -495,6 +501,7 @@ export class Monaco {
     eventStartTimestamp = 1924254038,
     marketLockTimestamp = 1924254038,
     eventStartOrderBehaviour: object = { cancelUnmatched: {} },
+    marketLockOrderBehaviour: object = { none: {} },
   ) {
     return await this.createMarketWithOptions({
       outcomes,
@@ -505,6 +512,7 @@ export class Monaco {
       eventStartTimestamp,
       marketLockTimestamp,
       eventStartOrderBehaviour,
+      marketLockOrderBehaviour,
     });
   }
 
@@ -812,6 +820,33 @@ export class MonacoMarket {
       });
   }
 
+  async cancelOrderPostMarketLock(orderPk: PublicKey) {
+    const [order] = await Promise.all([this.monaco.fetchOrder(orderPk)]);
+    const purchaserTokenPk = await this.cachePurchaserTokenPk(order.purchaser);
+    const matchingPoolPk = order.forOutcome
+      ? this.matchingPools[order.marketOutcomeIndex][order.expectedPrice]
+          .forOutcome
+      : this.matchingPools[order.marketOutcomeIndex][order.expectedPrice]
+          .against;
+    await this.monaco.program.methods
+      .cancelOrderPostMarketLock()
+      .accounts({
+        order: orderPk,
+        marketPosition: await this.cacheMarketPositionPk(order.purchaser),
+        purchaser: order.purchaser,
+        purchaserToken: purchaserTokenPk,
+        market: this.pk,
+        marketEscrow: this.escrowPk,
+        marketMatchingPool: matchingPoolPk,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  }
+
   async match(
     forOrderPk: PublicKey,
     againstOrderPk: PublicKey,
@@ -1039,6 +1074,25 @@ export class MonacoMarket {
           ? this.marketAuthority.publicKey
           : this.monaco.operatorPk,
         systemProgram: SystemProgram.programId,
+      })
+      .signers(this.marketAuthority ? [this.marketAuthority] : [])
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+        throw e;
+      });
+  }
+
+  async updateMarketLockTimeToNow() {
+    await this.monaco.program.methods
+      .updateMarketLocktimeToNow()
+      .accounts({
+        market: this.pk,
+        authorisedOperators:
+          await this.monaco.findMarketAuthorisedOperatorsPda(),
+        marketOperator: this.marketAuthority
+          ? this.marketAuthority.publicKey
+          : this.monaco.operatorPk,
       })
       .signers(this.marketAuthority ? [this.marketAuthority] : [])
       .rpc()
