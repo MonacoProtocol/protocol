6cf1b7b3222b4ecd4541c89a3258a0832bcdb84e
obi-ewan <ewan@betdex.com>
2023-12-05 12:48:01 +0000

    refactor: re-order type size fields

--- programs/monaco_protocol/src/state/type_size.rs
+++ programs/monaco_protocol/src/state/type_size.rs
@@ -1,19 +1,19 @@
 #![allow(dead_code)]
 
-pub const DISCRIMINATOR_SIZE: usize = 8;
 pub const BOOL_SIZE: usize = 1;
+pub const CHAR_SIZE: usize = 4;
+pub const DISCRIMINATOR_SIZE: usize = 8;
 pub const ENUM_SIZE: usize = 1; // for data/field-less enums
-pub const I64_SIZE: usize = 8;
-pub const I128_SIZE: usize = 16;
-pub const U128_SIZE: usize = 16;
-pub const U8_SIZE: usize = 1;
-pub const U16_SIZE: usize = 2;
-pub const U64_SIZE: usize = 8;
-pub const U32_SIZE: usize = 4;
 pub const F32_SIZE: usize = 4;
 pub const F64_SIZE: usize = 8;
+pub const I64_SIZE: usize = 8;
+pub const I128_SIZE: usize = 16;
 pub const PUB_KEY_SIZE: usize = 32;
-pub const CHAR_SIZE: usize = 4;
+pub const U8_SIZE: usize = 1;
+pub const U16_SIZE: usize = 2;
+pub const U32_SIZE: usize = 4;
+pub const U64_SIZE: usize = 8;
+pub const U128_SIZE: usize = 16;
 
 const OPTION_PREFIX_SIZE: usize = 1;
 pub const fn option_size(element_size: usize) -> usize {
