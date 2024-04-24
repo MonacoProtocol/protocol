d824ce819075d91433d43417f7bc9def7bae8f5f
Pawel Witek <pawel@betdex.com>
2024-04-24 23:52:15 +0100

    admin market close

--- admin/close_market.ts
+++ admin/close_market.ts
@@ -1,10 +1,14 @@
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
 import {
   findEscrowPda,
   findMarketMatchingQueuePda,
   findCommissionPaymentsQueuePda,
+  findAuthorisedOperatorsAccountPda,
+  Operator,
 } from "../npm-admin-client";
-import { getProtocolProgram } from "./util";
+import { getAnchorProvider, getProtocolProgram } from "./util";
+import { getMarket, getMarketMatchingQueue } from "../npm-client";
+import { Program } from "@coral-xyz/anchor";
 
 export async function closeMarket() {
   const protocolProgram = await getProtocolProgram();
@@ -15,23 +19,75 @@ export async function closeMarket() {
   }
 
   const marketPk = new PublicKey(process.argv[3]);
-  const authorityPk = new PublicKey(
-    "J2LqciLvyxVHMjMcda73459zWfFxw7rveDb5YAhSdGTe",
-  );
-  console.log(`Closing ${marketPk}`);
+  const authorityPk = getAnchorProvider().wallet.publicKey;
+  console.log(`Closing market ${marketPk} using authority ${authorityPk}`);
 
+  // check if market exists
+  const market = await getMarket(protocolProgram, marketPk);
+  if (!market.success) {
+    console.error(`Closing market ${marketPk} does not exist`);
+    return;
+  }
+
+  // check markets authority
+  const marketAuthorityPk = market.data.account.authority;
+  console.log(
+    `Closing market ${marketPk} expected authority ${marketAuthorityPk}`,
+  );
+  if (marketAuthorityPk != authorityPk) {
+    console.error(`Closing market ${marketPk} wrong authority`);
+    return;
+  }
+
+  const marketMatchingQueuePk = await findMarketMatchingQueuePda(
+    protocolProgram,
+    marketPk,
+  );
+  console.log(
+    `Closing market ${marketPk} expected matching queue ${marketMatchingQueuePk}`,
+  );
+
+  const marketMatchingQueue = await getMarketMatchingQueue(
+    protocolProgram,
+    marketMatchingQueuePk.data.pda,
+  );
+  if (!marketMatchingQueue.success) {
+    console.log(`Closing market ${marketPk} matching queue does not exist`);
+    const authorisedOperators = await findAuthorisedOperatorsAccountPda(
+      protocolProgram as Program,
+      Operator.MARKET,
+    );
+    await protocolProgram.methods
+      .initializeMarketQueues()
+      .accounts({
+        market: marketPk,
+        matchingQueue: marketMatchingQueuePk.data.pda,
+        marketOperator: getAnchorProvider().wallet.publicKey,
+        authorisedOperators: authorisedOperators.data.pda,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc()
+      .catch((e) => {
+        console.error(e);
+      });
+    console.log(`Closing market ${marketPk} initialize market queues`);
+  }
+
+  const marketEscrowPk = await findEscrowPda(protocolProgram, marketPk);
+  const marketCommissionPaymentsQueuePk = await findCommissionPaymentsQueuePda(
+    protocolProgram,
+    marketPk,
+  );
+
+  console.log(`Closing market ${marketPk} now`);
   await protocolProgram.methods
     .closeMarket()
     .accounts({
       market: marketPk,
       authority: authorityPk,
-      marketEscrow: (await findEscrowPda(protocolProgram, marketPk)).data.pda,
-      matchingQueue: (
-        await findMarketMatchingQueuePda(protocolProgram, marketPk)
-      ).data.pda,
-      commissionPaymentQueue: (
-        await findCommissionPaymentsQueuePda(protocolProgram, marketPk)
-      ).data.pda,
+      marketEscrow: marketEscrowPk.data.pda,
+      matchingQueue: marketMatchingQueuePk.data.pda,
+      commissionPaymentQueue: marketCommissionPaymentsQueuePk.data.pda,
     })
     .rpc()
     .catch((e) => {
