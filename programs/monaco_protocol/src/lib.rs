ebb7a19fdee62577e53204594d6170a07eb67462
Pawel Witek <pawel@betdex.com>
2024-09-27 17:19:02 +0100

    no edits

--- programs/monaco_protocol/src/lib.rs
+++ programs/monaco_protocol/src/lib.rs
@@ -20,6 +20,13 @@ pub mod events;
 pub mod instructions;
 pub mod state;
 
+#[cfg(feature = "stable")]
+declare_id!("5Q2hKsxShaPxFqgVtQH3ErTkiBf8NGb99nmpaGw7FCrr");
+#[cfg(feature = "dev")]
+declare_id!("yxvZ2jHThHQPTN6mGC8Z4i7iVBtQb3eBGeURQuLSrG9");
+#[cfg(feature = "edge")]
+declare_id!("mpDEVnZKneBb4w1vQsoTgMkNqnFe1rwW8qjmf3NsrAU");
+#[cfg(not(any(feature = "stable", feature = "dev", feature = "edge")))]
 declare_id!("monacoUXKtUi6vKsQwaLyxmXKSievfNWEcYXTgkbCih");
 
 #[program]
